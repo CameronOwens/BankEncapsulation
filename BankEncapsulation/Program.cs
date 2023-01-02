ea412f70bceb489472c6aa163cff719ea09507ee
CameronOwens <camowens58@gmail.com>
2023-01-02 01:29:03 -0500

    initial commit

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var account = new BankAccount();
+            Console.WriteLine("What is the amount you would like to deposit?");
+
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            account.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thank You! Your new balance is {account.GetBalance()}");
         }
     }
 }
